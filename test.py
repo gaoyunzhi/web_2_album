288de6fb0e06b5cab84118c152f07f66a925245d
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-16 08:12:20 -0700

    commit

--- test.py
+++ test.py
@@ -4,8 +4,10 @@
 import web_2_album
 
 def test():
+	# print(web_2_album.get(
+	# 	'http://weibointl.api.weibo.cn/share/131595305.html'))
 	print(web_2_album.get(
-		'http://weibointl.api.weibo.cn/share/131595305.html'))
+		'http://www.douban.com/people/zhuyige/status/2869326971/')
 	
 if __name__=='__main__':
 	test()
