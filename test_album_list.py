df89caf556b1561dd16ea9d8982fbe49212f458b
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-11 18:42:20 -0700

    commit

--- test_album_list.py
+++ test_album_list.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import cached_url
 from test import test
 
+start = False
+start_on = '1658225696'
+
 def findAlbumUrl(soup):
 	for album in soup.find_all('div', class_='title'):
 		a = album.find('a')
@@ -10,11 +13,17 @@ def findAlbumUrl(soup):
 		yield a['href']
 
 def process(root, total_page):
+	global start
+	
 	for page in range(0, total_page):
 		url = root + '?start=' + str(page * 25)
 		soup = BeautifulSoup(cached_url.get(url))
 		for album_url in findAlbumUrl(soup):
-			test(album_url)
+			if start_on in album_url:
+				start = True
+				continue
+			if start:
+				test(album_url)
 
 if __name__=='__main__':
 	process('https://www.douban.com/doulist/3589810/', 5)
